43a245dc082244b3a6b51ab5ebb22c32ccb1b7e7
Yuri Ershov <shtorm0606@gmail.com>
2022-01-14 18:18:35 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Yuri Ershov on 14.01.22.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
